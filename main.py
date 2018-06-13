8274402d25a7adb0f29271db96eb2b824917d781
Roman Lukin <me@rlukin.ru>
2018-06-13 22:42:46 +0300

    Add top level env wrapper

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from board import Board
 
 
-board = Board(size=30)
-board.set_snake([[x, 3] for x in range(4, 10)] + [[9, y] for y in range(4, 7)])
-board.set_fruits([[0, 1], [7, 5], [10, 23]])
-board.print()
+if __name__ == '__main__':
+    board = Board(size=30)
+    board.set_snake([[x, 3] for x in range(4, 10)] + [[9, y] for y in range(4, 7)])
+    board.set_fruits([[0, 1], [7, 5], [10, 23]])
+    board.print()
